920a8cff454997b32b02660b56146aa7c311b7aa
guynumeratorone <156556003+guynumeratorone@users.noreply.github.com>
2025-05-24 15:12:53 -0600

    Lab 05
    all test cases pass, lander spawns top right corner, has fuel, movement is flit to the correct direction.

---
+++ 230.05.Ponder Lab 05 done/velocity.cpp
@@ -0,0 +1,45 @@
+/***********************************************************************
+ * Source File:
+ *    VELOCITY
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    Everything we need to know about speed
+ ************************************************************************/
+
+#include "velocity.h"
+#include "acceleration.h"
+#include "angle.h"
+
+#include <math.h>  // for sqrt()
+
+ /*********************************************
+  * VELOCITY : ADD
+  *  v = v_0 + a t
+  *********************************************/
+void Velocity::add(const Acceleration& acceleration, double time)
+{
+   dx = dx + acceleration.getDDX() * time;
+   dy = dy + acceleration.getDDY() * time;
+}
+
+/*********************************************
+ * VELOCITY : GET SPEED
+ *  find the magnitude of velocity
+ *********************************************/
+double Velocity::getSpeed() const
+{  //           ___________
+   // speed = \/dx^2 + dy^2
+   return sqrt(dx * dx + dy * dy);
+}
+
+/*********************************************
+ * VELOCITY : SET
+ *  set from angle and direction
+ *  angle is in degrees
+ *********************************************/
+void Velocity::set(const Angle& angle, double magnitude)
+{
+   dx = magnitude * sin(angle.getRadians());  // horizontal
+   dy = magnitude * cos(angle.getRadians());  // vertical
+}
