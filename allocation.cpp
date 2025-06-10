b9bcab6d7832da3e2b45aead39816c208d129046
davidawells <wel20012@byui.edu>
2025-06-10 16:20:31 -0600

    beginning of allocation

---
+++ allocation.cpp
@@ -0,0 +1,14 @@
+float allocateOneFloat(float a) {
+    float* slot = a*;
+    return slot;
+}
+void deleteOneFloat(*float a) {
+    del a;
+}
+int[] allocateArrayDouble(int a) {
+    int[a] arr;
+    return arr;
+}
+void deleteArrayDouble(int[] a) {
+    del a[];
+}
