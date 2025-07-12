16cd15414a19b339865bc7ba543860d6c6264038
Josh Palmer <91572133+josh-palmerk@users.noreply.github.com>
2025-07-12 15:07:59 -0600

    ticket 4

--- Artillery/projectile.cpp
+++ Artillery/projectile.cpp
@@ -13,3 +13,35 @@
  using namespace std;
 
 
+ void Projectile::advance(double simulationTime)
+ {
+     // setup
+     const double gravity = -9.8;
+
+     if (flightPath.empty())
+         return;
+
+     PositionVelocityTime prev = flightPath.back();
+
+     // Calculate new velocity
+     Velocity newVelocity = prev.v;
+     newVelocity.addDy(gravity * simulationTime); // v = u + at
+
+     // Calculate new position
+     Position newPosition = prev.pos;
+     newPosition.addMetersX(prev.v.getDx() * simulationTime);
+     newPosition.addMetersY(prev.v.getDy() * simulationTime + 0.5 * gravity * simulationTime * simulationTime);
+
+     // Advance time
+     double newTime = prev.t + simulationTime;
+
+     // New PVT for flight path
+     PositionVelocityTime newPVT;
+     newPVT.pos = newPosition;
+     newPVT.v = newVelocity;
+     newPVT.t = newTime;
+
+     flightPath.push_back(newPVT);
+ }
+
+
